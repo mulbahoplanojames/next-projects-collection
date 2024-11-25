a8a8b79fb96af063a30ca6ef86d13b64bc058403
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-11-26 00:00:11 +0200

    Updated the repo  🗃️ by adding the dashoard3 Component

--- mul-dashboard/src/app/(dash)/dashboard3/page.tsx
+++ mul-dashboard/src/app/(dash)/dashboard3/page.tsx
@@ -1,10 +1,12 @@
 import DashboardHeader from "@/components/dash-header";
+import DashboardThreeCardWripper from "@/components/dashboard-three/DashboardThreeCardWripper";
 
 const Dashboar3 = () => {
   return (
     <>
       <section className="md:px-6 px-1 py-6">
         <DashboardHeader text="Dashboard 3" />
+        <DashboardThreeCardWripper />
       </section>
     </>
   );
