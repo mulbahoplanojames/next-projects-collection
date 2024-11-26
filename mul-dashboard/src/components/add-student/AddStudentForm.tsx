d8fdd2a5ad884e0298a8916c1700d4b14dee974a
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-11-26 16:42:29 +0200

    Updated the repo ⚱️ by adding Zod and react hook form for sommth form validation and experience

--- mul-dashboard/src/components/add-student/AddStudentForm.tsx
+++ mul-dashboard/src/components/add-student/AddStudentForm.tsx
@@ -1,13 +1,66 @@
-import React from "react";
 import { Card, CardContent, CardTitle } from "../ui/card";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { addStudentSchema } from "@/lib/Zod_Schema";
+// import { useForm,  } from "react-hook-form";
+
+import { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+type addStudentFormTypes = z.infer<typeof addStudentSchema>;
 
 const AddStudentForm = () => {
+  const form = useForm<addStudentFormTypes>({
+    resolver: zodResolver(addStudentSchema),
+  });
+
+  const onSubmit: SubmitHandler<addStudentFormTypes> = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
       <section className="md:px-6 px-1 py-6">
         <Card>
           <CardContent className="py-8">
             <CardTitle>Add Student</CardTitle>
+
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="first_name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>First Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
           </CardContent>
         </Card>
       </section>
---
+++ mul-dashboard/src/lib/Zod_Schema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const addStudentSchema = z.object({
+  first_name: z
+    .string()
+    .min(4, "First Name must be at least 3 characters long"),
+  last_name: z.string().min(3, "Last Name must be at least 3 characters long"),
+  roll_number: z
+    .string()
+    .min(8, "Roll Number must be at least 8 characters long"),
+  email: z.string().email("Invalid email address"),
+  registration_date: z.date().default(new Date()),
+  gender: z.string(),
+  class: z.string().min(3, "Class must be at least 3 characters long"),
+  phone: z.string(),
+  birth_date: z.date(),
+  blood_group: z.string(),
+  address: z.string(),
+  section: z.string().min(3, "Section must be at least 3 characters long"),
+});
