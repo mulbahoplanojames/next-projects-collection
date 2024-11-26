967968158ec4ae17fb173552d64dbdc1e1488900
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-11-26 15:48:14 +0200

    Updated the repo  🗃️ by adding the Add-students Component

---
+++ mul-dashboard/src/app/(dash)/add-student/page.tsx
@@ -0,0 +1,14 @@
+import DashboardHeader from "@/components/dash-header";
+import React from "react";
+
+const AddStudent = () => {
+  return (
+    <>
+      <section className="md:px-6 px-1 py-6">
+        <DashboardHeader text="Add Student" />
+      </section>
+    </>
+  );
+};
+
+export default AddStudent;
