e6e8ee9fc4c8d6cc5ab7ca206958579eeffb4f32
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-11-26 13:05:03 +0200

    Updated the repo  🗃️ by adding the dashoard3 Component

--- mul-dashboard/src/app/(dash)/dashboard3/page.tsx
+++ mul-dashboard/src/app/(dash)/dashboard3/page.tsx
@@ -1,4 +1,6 @@
 import DashboardHeader from "@/components/dash-header";
+import { ChartBar } from "@/components/dashboard-one/charts/bar-chart";
+import { ChartBar2 } from "@/components/dashboard-one/charts/bar-chart-2";
 import DashboardThreeCardWripper from "@/components/dashboard-three/DashboardThreeCardWripper";
 
 const Dashboar3 = () => {
@@ -7,6 +9,10 @@ const Dashboar3 = () => {
       <section className="md:px-6 px-1 py-6">
         <DashboardHeader text="Dashboard 3" />
         <DashboardThreeCardWripper />
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-10 mt-12 md:px-1">
+          <ChartBar2 text={"Students Performance"} />
+          <ChartBar text={"Teachers Overview"} />
+        </div>
       </section>
     </>
   );
--- mul-dashboard/src/components/dashboard-three/DashboardThreeCardWripper.tsx
+++ mul-dashboard/src/components/dashboard-three/DashboardThreeCardWripper.tsx
@@ -1,6 +1,5 @@
 import cardsData from "@/data/dashboardThree/data";
 import { dashThreeCardTypes } from "@/types/dashboardsTypes";
-import { Icon } from "lucide-react";
 import React from "react";
 
 const DashboardThreeCardWripper = () => {
@@ -11,15 +10,19 @@ const DashboardThreeCardWripper = () => {
           {cardsData.map((data: dashThreeCardTypes) => (
             <div className="flex items-center gap-3" key={data.label}>
               <div
-                className="w-14 h-14 rounded-full  flex items-center justify-center"
+                className="lg:w-14 lg:h-14 h-10 w-10 rounded-full  flex items-center justify-center"
                 style={{ background: `${data.color}` }}
               >
                 {/* {data.icon} */}
-                <data.icon size={32} className="text-white" />
+                <data.icon size={28} className="text-white" />
               </div>
               <div>
-                <h1 className="md:text-xl text-base">{data.label}</h1>
-                <p className="md:text-2xl text-lg font-bold">{data.count}</p>
+                <h1 className="lg:text-xl text-base text-black">
+                  {data.label}
+                </h1>
+                <p className="lg:text-2xl text-lg font-bold text-black">
+                  {data.count}
+                </p>
               </div>
             </div>
           ))}
