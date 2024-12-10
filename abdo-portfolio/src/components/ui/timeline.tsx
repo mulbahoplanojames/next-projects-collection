eed4d0f9048f4d13f4a5dbd6524f4198f25b0225
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-12-10 13:51:22 +0200

    Updated the 🗄️, working on the Experience Component

--- abdo-portfolio/src/components/ui/timeline.tsx
+++ abdo-portfolio/src/components/ui/timeline.tsx
@@ -34,17 +34,17 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
 
   return (
-    <div
-      className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
-      ref={containerRef}
-    >
+    <div className="w-full font-sans md:px-10 " ref={containerRef}>
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl">
-          Changelog from my journey
+        <h2 className="text-lg md:text-4xl mb-4 text-text_primary max-w-4xl">
+          My Professional Experience
         </h2>
-        <p className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
+        <p className="text-text_primary  text-sm md:text-base max-w-lg">
+          I have had the privilege of working on a wide range of projects,
+          including web development, mobile app development, and software
+          development. My experience in these fields has allowed me to develop a
+          deep understanding of the latest technologies and best practices in
+          the industry.
         </p>
       </div>
 
